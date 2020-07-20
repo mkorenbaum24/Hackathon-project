1f182ef38bf05cdce9101aa4c7d80c6b8fc15a1b
mkorenbaum24 <maxdefense58@icloud.com>
2020-07-20 12:57:05 -0400

    added some psuedocode

---
+++ assets/project.js
@@ -0,0 +1,16 @@
+// first create the player
+
+//then we should add the controls for the arrow keys to move the player
+//
+
+//know when specific key is pressed so it can tell wherether to move left up down or right.
+
+
+//add the computer/enemy player
+
+//add the computer movements.
+
+//If the player touches the enemy the dies an lose a life.
+
+//respawn if lives is greater than zero.
+//If lives === 0, then game over.
