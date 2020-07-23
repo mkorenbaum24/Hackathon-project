c4715126738d3f8e22569f568baa7e57881ec268
Daniel Gassenheimer <danielgassenheimer@MacBook-Air.local>
2020-07-23 13:11:58 -0400

    save file

--- assets/project.js
+++ assets/project.js
@@ -10,133 +10,133 @@ $(document).ready(function () {
     let startX = 10
     let startY = 200
     function startGame() {
-      myGamePiece = new Component(50, 50, "assets/mario.png", startX, startY, "image")
-      myObstacle = new Component(20, 20, "red", 510, 230)
-      lives = 3
-      myGameArea.start()
+        myGamePiece = new Component(50, 50, "assets/mario.png", startX, startY, "image")
+        myObstacle = new Component(20, 20, "red", 510, 230)
+        lives = 3
+        myGameArea.start()
     }
     function fadeOut(imageNumber) {
-      var targetId = $(`#lives-${imageNumber}`)
-      console.log(imageNumber)
-      targetId.animate({ opacity: "0.10" })
+        var targetId = $(`#lives-${imageNumber}`)
+        console.log(imageNumber)
+        targetId.animate({ opacity: "0.10" })
     }
     $(".up-button").mousedown("click", function () {
-      moveup()
+        moveup()
     })
     $(".down-button").mousedown("click", function () {
-      movedown()
+        movedown()
     })
     $(".left-button").mousedown("click", function () {
-      moveleft()
+        moveleft()
     })
     $(".right-button").mousedown("click", function () {
-      moveright()
+        moveright()
     })
     $(".btn").mouseup(function () {
-      clearmove()
+        clearmove()
     })
     var myGameArea = {
-      canvas: document.getElementById("myCanvas"),
-      start: function () {
-        this.canvas.width = 600
-        this.canvas.height = 450
-        this.context = this.canvas.getContext("2d")
-        // document.body.insertBefore(this.canvas, document.body.childNodes[0])
-        this.frameNo = 0
-        this.interval = setInterval(updateGameArea, 20)
-      },
-      clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-      },
-      stop: function () {
-        clearInterval(this.interval)
-      },
+        canvas: document.getElementById("myCanvas"),
+        start: function () {
+            this.canvas.width = 600
+            this.canvas.height = 450
+            this.context = this.canvas.getContext("2d")
+            // document.body.insertBefore(this.canvas, document.body.childNodes[0])
+            this.frameNo = 0
+            this.interval = setInterval(updateGameArea, 20)
+        },
+        clear: function () {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        },
+        stop: function () {
+            clearInterval(this.interval)
+        },
     }
     function Component(width, height, color, x, y, type) {
-      this.type = type
-      if (type == "image") {
-        this.image = new Image()
-        this.image.src = color
-      }
-      this.width = width
-      this.height = height
-      this.speedX = 0
-      this.speedY = 0
-      this.x = x
-      this.y = y
-      this.update = function () {
-        ctx = myGameArea.context
+        this.type = type
         if (type == "image") {
-          ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-        } else {
-          ctx.fillStyle = color
-          ctx.fillRect(this.x, this.y, this.width, this.height)
+            this.image = new Image()
+            this.image.src = color
         }
-      }
-      this.newPos = function () {
-        this.x += this.speedX
-        this.y += this.speedY
-      }
-      this.resetPos = function (newX, newY) {
-        this.x = newX
-        this.y = newY
-      }
-      this.crashWith = function (otherobj) {
-        var myleft = this.x
-        var myright = this.x + this.width
-        var mytop = this.y
-        var mybottom = this.y + this.height
-        var otherleft = otherobj.x
-        var otherright = otherobj.x + otherobj.width
-        var othertop = otherobj.y
-        var otherbottom = otherobj.y + otherobj.height
-        var crash = true
-        if (mybottom < othertop || mytop > otherbottom || myright < otherleft || myleft > otherright) {
-          crash = false
+        this.width = width
+        this.height = height
+        this.speedX = 0
+        this.speedY = 0
+        this.x = x
+        this.y = y
+        this.update = function () {
+            ctx = myGameArea.context
+            if (type == "image") {
+                ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+            } else {
+                ctx.fillStyle = color
+                ctx.fillRect(this.x, this.y, this.width, this.height)
+            }
         }
-        return crash
-      }
+        this.newPos = function () {
+            this.x += this.speedX
+            this.y += this.speedY
+        }
+        this.resetPos = function (newX, newY) {
+            this.x = newX
+            this.y = newY
+        }
+        this.crashWith = function (otherobj) {
+            var myleft = this.x
+            var myright = this.x + this.width
+            var mytop = this.y
+            var mybottom = this.y + this.height
+            var otherleft = otherobj.x
+            var otherright = otherobj.x + otherobj.width
+            var othertop = otherobj.y
+            var otherbottom = otherobj.y + otherobj.height
+            var crash = true
+            if (mybottom < othertop || mytop > otherbottom || myright < otherleft || myleft > otherright) {
+                crash = false
+            }
+            return crash
+        }
     }
     function updateGameArea() {
-      if (myGamePiece.crashWith(myObstacle)) {
-        myGameArea.stop()
-        fadeOut(lives)
-        if (lives > 1 && lives < 4) {
-          lives--
-          myGamePiece.resetPos(startX, startY)
-          alert(lives + " left!")
-          myGameArea.start()
-        } // if lives < 1 then game over ...
-        if (lives <= 1) {
-          alert("Game Over!!!")
-          // game over -- show it
-          // reset game --
+        if (myGamePiece.crashWith(myObstacle)) {
+            myGameArea.stop()
+            fadeOut(lives)
+            if (lives > 1 && lives < 4) {
+                lives--
+                myGamePiece.resetPos(startX, startY)
+                alert(lives + " left!")
+                myGameArea.start()
+            } // if lives < 1 then game over ...
+            else {
+                alert("Game Over!!!")
+                // game over -- show it
+                // reset game --
+            }
+            /* document.getElementById("notifications").textContent = "Collision!!!"*/
+            // alert("collision!!");
+        } else {
+            myGameArea.clear()
+            myGamePiece.newPos()
+            myGamePiece.update()
+            myObstacle.newPos()
+            myObstacle.update()
         }
-        /* document.getElementById("notifications").textContent = "Collision!!!"*/
-        // alert("collision!!");
-      } else {
-        myGameArea.clear()
-        myGamePiece.newPos()
-        myGamePiece.update()
-        myObstacle.newPos()
-        myObstacle.update()
-      }
     }
     function moveup() {
-      myGamePiece.speedY = -1
+        myGamePiece.speedY = -1
     }
     function movedown() {
-      myGamePiece.speedY = 1
+        myGamePiece.speedY = 1
     }
     function moveleft() {
-      myGamePiece.speedX = -1
+        myGamePiece.speedX = -1
     }
     function moveright() {
-      myGamePiece.speedX = 1
+        myGamePiece.speedX = 1
     }
     function clearmove() {
-      myGamePiece.speedX = 0
-      myGamePiece.speedY = 0
+        myGamePiece.speedX = 0
+        myGamePiece.speedY = 0
     }
     startGame()
     // CEDRICS CODE
@@ -158,9 +158,8 @@ $(document).ready(function () {
            alert("gameover")
        }
    */
-  })
+})
 
-  
-  
-  
-  
+
+
+
